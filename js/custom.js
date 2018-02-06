a18c7d9431844331b173618c5b6076328dbc5fb3
liuchibing <liuchibing@outlook.com>
2018-02-06 22:49:57 +0800

    创建导航条brand监听滚动进行隐藏显示的动画

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    var navbarHeight = document.getElementsByClassName("navbar")[0].getClientRects()[0].height;
+    var boundingClientRect = document.getElementById("large-brand").getBoundingClientRect();
+    ScrollPosStyler.init({
+        scrollOffsetY: (boundingClientRect.top + boundingClientRect.bottom) / 2 - navbarHeight
+    });
 })
